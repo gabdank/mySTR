58cd02c3084c69cd58bdf80dccdd7764869fe135
Idan Gabdank <gabdank@stanford.edu>
2015-03-06 16:56:13 -0800

    thread tested sucessfully for configuration involving flanks and no flanks repeats

--- src/domain/multiThreading/processingThread.java
+++ src/domain/multiThreading/processingThread.java
@@ -116,14 +116,16 @@ public class processingThread implements Runnable{
 					id = tokenizer.nextToken();
 				}
 				if (a==2){ // we should examine the sequence
-					processRead(line1, line2, id, shortKmerMap, longKmerMap, repeatUnitLocationMap,
+					//if (id.equals("chr1.AG-3248/1")){
+						processRead(line1, line2, id, shortKmerMap, longKmerMap, repeatUnitLocationMap,
 							kmerIndexMap, repeatIndexingMap, find_repetition,
 							allMaxScoresDistribution,
 							allUsedScoresDistribution,repetitionList);
-					processRead(line2, line1, id, shortKmerMap, longKmerMap, repeatUnitLocationMap,
+						processRead(line2, line1, id, shortKmerMap, longKmerMap, repeatUnitLocationMap,
 							kmerIndexMap, repeatIndexingMap, find_repetition,
 							allMaxScoresDistribution,
 							allUsedScoresDistribution,repetitionList);
+					//}
 					//break;
 				}
 				if (a==4){
@@ -152,20 +154,25 @@ public class processingThread implements Runnable{
 			int[] allMaxScoresDistribution, int[] allUsedScoresDistribution, RepetitionList repetition1)
 					throws IOException {
 
-		//System.out.println("processing read1:"+line1);
+		System.out.println("processing read1:"+line1);
 		repetition1.emptyRepetitionList();
 		find_repetition.findMaximalRepetition(line1,repetition1);
+		
 		find_repetition.extendMaximalRepetition(id, line1, repetition1);
-
+		//System.out.println("unit size : "+repetition1.get_max_repetition_unit_size());
+		//System.out.println("length of the repetition : "+repetition1.get_max_repetition_length());
 		// Ensure we are dealing with short tandem repeat (2..8bp long) and that the repeat length is at least 16bp long 
 		if (repetition1.get_max_repetition_unit_size()>1 && repetition1.get_max_repetition_unit_size()<9 && repetition1.get_max_repetition_length()>16){
 			String repeat1 = line1.substring(repetition1.get_max_repetition_left_index(),repetition1.get_max_repetition_left_index()+repetition1.get_max_repetition_unit_size());
+			//System.out.println("Found repetition:"+repeat1);
 			if (!repeat1.contains("N")){
+				//System.out.println("No Ns in the repeat :)");
 				String representativeRepeat1 = SequenceUtils.getRepresentative(repeat1);
+				
 				int repeatIndex1 = repeatIndexingMap.getIntForString(representativeRepeat1);
 				ArrayList<GenomicLocation> genomicLocations1 = repMap.get(repeatIndex1); 
 				if (genomicLocations1 != null){
-
+					//System.out.println("There are some potential genomic locatons");
 					String leftFlank = line1.substring(0,repetition1.get_max_repetition_left_index());
 					String rightFlank = line1.substring(repetition1.get_max_repetition_right_index());
 
@@ -180,10 +187,10 @@ public class processingThread implements Runnable{
 						rightFlank = rightFlank.substring(0,30);
 					}
 
-					/*System.out.println("LEFT FLANK = "+leftFlank);
-					System.out.println("RIGHT FLANK = "+rightFlank);
-					System.out.println("REPEAT = "+repeatSection);
-					 */
+					//System.out.println("LEFT FLANK = "+leftFlank);
+					//System.out.println("RIGHT FLANK = "+rightFlank);
+					//System.out.println("REPEAT = "+repeatSection);
+					
 					TIntIntMap readFlankKmers = new TIntIntHashMap(); // map that will store all kmers coming from short flanks
 					TIntIntMap readPairKmers = new TIntIntHashMap(); // map that will store all kmers from the paired End read
 
@@ -195,12 +202,17 @@ public class processingThread implements Runnable{
 					
 					for (int p=0;p<leftFlank.length()-11;p=p+1){
 						String twelveMer = leftFlank.substring(p,p+12);
+						//System.out.println("left flank Kmer "+twelveMer);
 						if (!twelveMer.contains("N")){
 							int kmerIndex = kmerIndexMap.getIntForString(twelveMer);
-
+							//System.out.println("left flank Kmer index "+kmerIndex);
 							kmerUnitPairLocations = shortKmerMap.get(new KmerRepeatUnitPair(kmerIndex, repeatIndex1));
-
-							if (kmerUnitPairLocations != null){										
+							//System.out.println("Pair = " + new KmerRepeatUnitPair(kmerIndex, repeatIndex1));
+							
+							
+							
+							if (kmerUnitPairLocations != null){
+								//System.out.println("Num of locatoins of the pair "+kmerUnitPairLocations.size());
 								if (!readFlankKmers.containsKey(kmerIndex)){
 									for (GenomicLocation gl : kmerUnitPairLocations){
 										gl.addOneToCounter(threadID);												
@@ -211,13 +223,16 @@ public class processingThread implements Runnable{
 
 						}
 					}
-
+					//System.out.println("number of flank kmers detected was "+readFlankKmers.size());
 					// same story with right flank
 					
 					for (int p=0;p<rightFlank.length()-11;p=p+1){
 						String twelveMer = rightFlank.substring(p,p+12);
+						//System.out.println("right flank Kmer "+twelveMer);
+
 						if (!twelveMer.contains("N")){	
 							int kmerIndex = kmerIndexMap.getIntForString(twelveMer);
+							//System.out.println("Pair = " + new KmerRepeatUnitPair(kmerIndex, repeatIndex1));
 
 							kmerUnitPairLocations = shortKmerMap.get(new KmerRepeatUnitPair(kmerIndex, repeatIndex1));
 							if (kmerUnitPairLocations != null){							
@@ -230,7 +245,7 @@ public class processingThread implements Runnable{
 							}
 						}
 					}
-
+					//System.out.println("number of flank kmers detected was "+readFlankKmers.size());
 
 										
 					// MAXIMUM FINDING	
@@ -243,8 +258,9 @@ public class processingThread implements Runnable{
 
 					int numOfLocations = genomicLocations1.size();
 					int[] scoresOfLocations=new int[numOfLocations];
-
-					if (maxNumOfFlankKmers>20){// we can relay solely on the short flank kmers - because there are enough of these
+					
+					//System.out.println("Number of k-mers was:"+maxNumOfFlankKmers);
+					if (maxNumOfFlankKmers>15){// we can relay solely on the short flank kmers - because there are enough of these
 						int ind = 0;
 						for (GenomicLocation gl : genomicLocations1){
 							int localKmerCount = gl.getCounter(threadID);
@@ -275,9 +291,9 @@ public class processingThread implements Runnable{
 						// ensuring fast count and selection of potential Mappings
 						// we should not forget the kmers counts coming from the flanks - they are not many - but still there
 						// we shouldn't attemp aligning without taking into consideration the short flanks sequence kmers
+						//System.out.println("Going through the paired end - since flanks were not enough informative");
 						
-						
-						for (int p=0;p<mateRead.length()-11;p=p+5){
+						for (int p=0;p<mateRead.length()-11;p=p+1){
 							String twelveMer = mateRead.substring(p,p+12);
 							if (!twelveMer.contains("N")){
 								int kmerIndex = kmerIndexMap.getIntForString(twelveMer);
@@ -293,7 +309,9 @@ public class processingThread implements Runnable{
 							}
 						}
 						maxNumOfPairKmers = readPairKmers.keySet().size();
-							
+						
+						//System.out.println("Maximum number of kmers for the pair end: "+maxNumOfPairKmers);
+						
 						maxScore = -1.0;
 						if (maxNumOfFlankKmers+maxNumOfPairKmers>20){// we can relay on mixture of flank Kmers and paired end kmers
 							int ind = 0;
@@ -387,7 +405,7 @@ public class processingThread implements Runnable{
 			// TODO align and get the alignment score
 			// TODO decide later what to do with it
 			if (validLengths(leftFlank, rightFlank,gl.getLeftFlank(), gl.getRightFlank())==true){
-				System.out.println(align(leftFlank, rightFlank, repetitiveSection, gl.getLeftFlank(), gl.getRightFlank(), gl.getRepetitiveSection(), pairRead, matrix,	gapP, readID));
+				//System.out.println(align(leftFlank, rightFlank, repetitiveSection, gl.getLeftFlank(), gl.getRightFlank(), gl.getRepetitiveSection(), pairRead, matrix,	gapP, readID));
 				allignmentDataList.add(align(leftFlank, rightFlank, repetitiveSection, gl.getLeftFlank(), gl.getRightFlank(), gl.getRepetitiveSection(), pairRead, matrix,	gapP, readID));
 			}
 		}
@@ -409,12 +427,12 @@ public class processingThread implements Runnable{
 				maximumsCounter++;
 			}
 		}
-		System.out.println("------------------");		
-		System.out.println("Number of maximums:"+maximumsCounter);
-		System.out.println("Maximal score = "+maximalScore);
-		System.out.println("------------------");
+		//System.out.println("------------------");		
+		//System.out.println("Number of maximums:"+maximumsCounter);
+		//System.out.println("Maximal score = "+maximalScore);
+		//System.out.println("------------------");
 		
-		if (maximumsCounter==1 && maximalScore>=0.9){			
+		if (maximumsCounter==1 && maximalScore>=0.85){			
 			bw.write(potentialLocations.get(maximalIndex).reportTheGenomicLocation()+"{[");
 			bw.write(allignmentDataList.get(maximalIndex).reportTheAlignmentData()+"]#}\n");							
 		}
@@ -427,18 +445,26 @@ public class processingThread implements Runnable{
 		// forward alignment
 		int rLeftFlankLen = readLeftFlank.length();
 		int rRightFlankLen = readRightFlank.length();
+		//System.out.println("STARTING TO ALIGN:");
+		//System.out.println();
+		//System.out.println(readLeftFlank+" Left length "+rLeftFlankLen);
+		//System.out.println(readRightFlank+" Right length "+rRightFlankLen);
 					
 		DNASequence readLeft = new DNASequence(readLeftFlank+readRepetitiveRegion.substring(0,8), AmbiguityDNACompoundSet.getDNACompoundSet());
 		DNASequence readRight = new DNASequence(readRepetitiveRegion.substring(readRepetitiveRegion.length()-8)+readRightFlank, AmbiguityDNACompoundSet.getDNACompoundSet());
 		
-		int genomicLeftLen = readLeft.getLength();
-		int genomicRightLen = readRight.getLength();
+		//int genomicLeftLen = readLeft.getLength();
+		//int genomicRightLen = readRight.getLength();
 		
 			
 		DNASequence genomicLeft = new DNASequence(genomicLeftFlank.substring(genomicLeftFlank.length()-rLeftFlankLen)+genomicRepetitiveRegion.substring(0,8), AmbiguityDNACompoundSet.getDNACompoundSet());
 		DNASequence genomicRight = new DNASequence(genomicRepetitiveRegion.substring(genomicRepetitiveRegion.length()-8)+genomicRightFlank.substring(0,rRightFlankLen), AmbiguityDNACompoundSet.getDNACompoundSet());
+		
 		SequencePair<DNASequence, NucleotideCompound> leftSideForward =Alignments.getPairwiseAlignment(readLeft, genomicLeft,PairwiseSequenceAlignerType.GLOBAL, gap,mat);
 		SequencePair<DNASequence, NucleotideCompound> rightSideForward =Alignments.getPairwiseAlignment(readRight, genomicRight,PairwiseSequenceAlignerType.GLOBAL, gap,mat);
+		
+		//System.out.println("left fwd :\n"+leftSideForward);
+		//System.out.println("right fwd :\n"+rightSideForward);
 		//reverse alignment
 		String reverseReadLeftSeq = SequenceUtils.getReverseComplementary(readLeftFlank);
 		String reverseReadRepetitiveSeq = SequenceUtils.getReverseComplementary(readRepetitiveRegion);
@@ -463,29 +489,103 @@ public class processingThread implements Runnable{
 		SequencePair<DNASequence, NucleotideCompound> pairedLeft =Alignments.getPairwiseAlignment(pairedRead, genomicLeftFull,PairwiseSequenceAlignerType.LOCAL, gap,mat);
 		SequencePair<DNASequence, NucleotideCompound> pairedRight =Alignments.getPairwiseAlignment(reversePairedRead, genomicRightFull,PairwiseSequenceAlignerType.LOCAL, gap,mat);
 
+		System.out.println("Left pair \n"+pairedLeft);
+		System.out.println((double)pairedLeft.getNumIdenticals()/pairedEnd.length());
+		System.out.println("Right pair \n"+pairedRight);
+		System.out.println((double)pairedRight.getNumIdenticals()/pairedEnd.length());
 		/*
 		 * assuming that everything worked fine we can now evaluate our alignments
 		 * so there are two main cases - forward flank and reverse paired end or reversed flanks and forward paired end
 		 */
 		
-		int totalForwardFlanksLength = leftSideForward.getLength()+ rightSideForward.getLength();
-		int totalForwardFlanksIdenticals = leftSideForward.getNumIdenticals()+ rightSideForward.getNumIdenticals();
-
-		int totalReverseFlanksLength = leftSideReverse.getLength()+ rightSideReverse.getLength();
-		int totalReverseFlanksIdenticals = leftSideReverse.getNumIdenticals()+ rightSideReverse.getNumIdenticals();
-
+		int totalForwardFlanksLength=-1;
+		int totalForwardFlanksIdenticals=-1;
+		int totalReverseFlanksLength=-1;
+		int totalReverseFlanksIdenticals=-1;
+		
+		int flankSizeLimit = 5;
+		boolean noFlanks = false;
+		
+		if (rLeftFlankLen>=flankSizeLimit && rRightFlankLen>=flankSizeLimit){
+			totalForwardFlanksLength = leftSideForward.getLength()+ rightSideForward.getLength();
+			totalForwardFlanksIdenticals = leftSideForward.getNumIdenticals()+ rightSideForward.getNumIdenticals();
+			totalReverseFlanksLength = leftSideReverse.getLength()+ rightSideReverse.getLength();
+			totalReverseFlanksIdenticals = leftSideReverse.getNumIdenticals()+ rightSideReverse.getNumIdenticals();
+		}
+		else{
+			if (rLeftFlankLen>=flankSizeLimit && rRightFlankLen<flankSizeLimit){
+				System.out.println("left is ok, right is small");
+				
+				totalForwardFlanksLength = leftSideForward.getLength();
+				totalForwardFlanksIdenticals = leftSideForward.getNumIdenticals();
+					
+				totalReverseFlanksLength = rightSideReverse.getLength();
+				totalReverseFlanksIdenticals = rightSideReverse.getNumIdenticals();
+				
+				System.out.println(leftSideForward);
+				System.out.println("totalFwdFlankLength : "+totalForwardFlanksLength);
+				System.out.println("totalFwdFlankIdenticals : "+totalForwardFlanksIdenticals);
+				
+				System.out.println(rightSideReverse);
+				System.out.println("totalRevFlankLength : "+totalReverseFlanksLength);
+				System.out.println("totalRevFlankIdenticals : "+totalReverseFlanksIdenticals);
+			}
+			else{
+				if (rLeftFlankLen<flankSizeLimit && rRightFlankLen>=flankSizeLimit){
+					System.out.println("left is small, right is ok");
+					
+					totalForwardFlanksLength = rightSideForward.getLength();
+					totalForwardFlanksIdenticals = rightSideForward.getNumIdenticals();
+					
+					totalReverseFlanksLength = leftSideReverse.getLength();
+					totalReverseFlanksIdenticals = leftSideReverse.getNumIdenticals();
+					
+					System.out.println(rightSideForward);
+					System.out.println("totalFwdFlankLength : "+totalForwardFlanksLength);
+					System.out.println("totalFwdFlankIdenticals : "+totalForwardFlanksIdenticals);
+					
+					System.out.println(leftSideReverse);
+					System.out.println("totalRevFlankLength : "+totalReverseFlanksLength);
+					System.out.println("totalRevFlankIdenticals : "+totalReverseFlanksIdenticals);
+					
+				}
+				else{
+					if(rLeftFlankLen<flankSizeLimit && rRightFlankLen<flankSizeLimit){
+						//System.out.println("no flanks");
+						noFlanks = true;
+					}
+				}
+			}
+		}
+	
 		int totalPairedReadLength = pairedEnd.length();
 		int totalPairedEndLeftIdenticals = pairedLeft.getNumIdenticals();
 		int totalPairedEndRightIdenticals = pairedRight.getNumIdenticals();
+		
+		//System.out.println("forward paired end:\n"+pairedRight+(double)totalPairedEndRightIdenticals/totalPairedReadLength);
+		
+		//System.out.println("reverse paired end:\n"+pairedLeft+(double)totalPairedEndLeftIdenticals/totalPairedReadLength);
 
-
-		double forwardScoreMin = Math.min((double)totalForwardFlanksIdenticals/totalForwardFlanksLength,(double)totalPairedEndRightIdenticals/totalPairedReadLength);
-		double reverseScoreMin = Math.min((double)totalReverseFlanksIdenticals/totalReverseFlanksLength ,(double)totalPairedEndLeftIdenticals/totalPairedReadLength);
+		double forwardScoreMin;
+		double reverseScoreMin;
+		if (noFlanks==false){
+			forwardScoreMin = Math.min((double)totalForwardFlanksIdenticals/totalForwardFlanksLength,(double)totalPairedEndRightIdenticals/totalPairedReadLength);
+			reverseScoreMin = Math.min((double)totalReverseFlanksIdenticals/totalReverseFlanksLength ,(double)totalPairedEndLeftIdenticals/totalPairedReadLength);
+		}else{
+			//System.out.println("aligning only paired end sionce the flanks are too short");
+			forwardScoreMin=(double)totalPairedEndRightIdenticals/totalPairedReadLength;
+			reverseScoreMin=(double)totalPairedEndLeftIdenticals/totalPairedReadLength;
+		}
+		
+		//System.out.println();
+		//System.out.println("FINISHED TO ALIGN:");
 
 		if (forwardScoreMin>reverseScoreMin){
+			System.out.println("Forward wins");
 			return new AlignmentsData(forwardScoreMin, id, readLeftFlank, readRightFlank, readRepetitiveRegion, leftSideForward, rightSideForward, pairedRight);
 		}
 		else{
+			System.out.println("Reverse wins");
 			return new AlignmentsData(reverseScoreMin, id, reverseReadRightSeq, reverseReadLeftSeq, reverseReadRepetitiveSeq, leftSideReverse, rightSideReverse, pairedLeft);
 		}
 	}	
--- src/test/ThreadTest.java
+++ src/test/ThreadTest.java
@@ -3,9 +3,13 @@ package test;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import org.junit.Test;
 
+import domain.GenomicLocation;
+import domain.KmerRepeatUnitPair;
 import domain.kmerInformator;
 import domain.multiThreading.processingThread;
 
@@ -13,8 +17,13 @@ public class ThreadTest {
 
 	@Test
 	public void test() throws IOException {
-		kmerInformator testInformator = new kmerInformator(12, "/home/gabdank/Documents/STR_Attempt/Simulation/chromosomes.list", "/home/gabdank/Documents/STR_Attempt/Simulation/sample.indexed.bed", 13);
-		processingThread t = new processingThread(5,"/home/gabdank/Documents/STR_Attempt/Simulation/paired_dat1.fastq","/home/gabdank/Documents/STR_Attempt/Simulation/paired_dat2.fastq", testInformator,"/home/gabdank/Documents/STR_Attempt/Simulation/out_") ;
+		kmerInformator testInformator = new kmerInformator(12, "/home/gabdank/Documents/STR_Attempt/Simulation/chromosomes.list", "/home/gabdank/Documents/STR_Attempt/Simulation/one.indexed.bed", 13);
+		HashMap<KmerRepeatUnitPair, HashSet<GenomicLocation>> z = testInformator.getImmidiateFlanksKmerRepUnit2SetOfLocations();
+		/*for (KmerRepeatUnitPair p : z.keySet()){
+			System.out.println(p+"\t"+z.get(p));
+		}*/
+		
+		processingThread t = new processingThread(5,"/home/gabdank/Documents/STR_Attempt/Simulation/case7_1.fastq","/home/gabdank/Documents/STR_Attempt/Simulation/case7_2.fastq", testInformator,"/home/gabdank/Documents/STR_Attempt/Simulation/out_") ;
 		t.run();
 	}
 }
