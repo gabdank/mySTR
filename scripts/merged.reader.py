a8ff575b31751ca235ffa67aca59c233d85eb7a1
idan <gabdank@stanford.edu>
2015-03-10 09:14:55 -0700

    aaaa

--- scripts/merged.reader.py
+++ scripts/merged.reader.py
@@ -67,14 +67,120 @@ def processReferenceData(data, threshold, delta):
     return (array[2].split(":")[1][-threshold:], actualRepeat, numOfReps ,unfinishedBusiness, array[3].split(":")[1][0:threshold])
 
    
+def processNumbers(strainFile, locationsDictionary, strainName, strainSpecificDiction):
+    m = 0
+    currentRecordKey = ""
+    fil = open(strainFile, "r")
+    #The expectation is to get file of the following format:
+    '''
+    repetitiveUnit:
+    {
+    [leftSequence:XXX$...]
+    []
+    []
+    []
+    }
+    repetitiveUnit:    
+    
+    
+    '''
+    for l in fil:
+        if l[0]=='r': # we are dealing with beginning of location
+            array = l.strip()[:-1].split('$')
+            #print array
+            #print array[-3]
+            #print array[-2]
+            currentRecordKey = (array[-3].split(":")[1],array[-2].split(":")[1])
+            print currentRecordKey
+            #print locationsDictionary[currentRecordKey]
+        #if l[0]=='{': #starting section of aligned reads
+        #if l[0]=='}': #finished to read section of aligned reads
+        if l[0]=="[":
+            print l[1:-2].split('$')
+            
+        '''m+=1
+        n = 0
+        if l[0:14]=="repetitiveUnit":
+            if len(currentRecord)>0:
+                locationsDictionary[getKey(currentRecord[0:-1])][strainName]=getNumbers(currentRecord[0:-1])
+                refNumOfReps = locationsDictionary[getKey(currentRecord[0:-1])]['reference'][2]
+                listExacts = locationsDictionary[getKey(currentRecord[0:-1])][strainName][0]
+                listLowers = locationsDictionary[getKey(currentRecord[0:-1])][strainName][1]
 
+                #if isInteresting(listExacts, listLowers, refNumOfReps)== True:
+                #    strainSpecificDiction[getKey(currentRecord[0:-1])]=currentRecord
+
+                currentRecord = l.strip()
+            else:
+                currentRecord = l.strip()+"$"
+        else:
+            currentRecord+= l.strip()+"$"
+        '''
+    #locationsDictionary[getKey(currentRecord)][strainName]=getNumbers(currentRecord)
+    #refNumOfReps = locationsDictionary[getKey(currentRecord)]['reference'][2]
+    #listExacts =  locationsDictionary[getKey(currentRecord)][strainName][0]
+    #listLowers =  locationsDictionary[getKey(currentRecord)][strainName][1]
+   
+    #if isInteresting(listExacts, listLowers, refNumOfReps)== True:
+    #    strainSpecificDiction[getKey(currentRecord)]=currentRecord
+
+    fil.close()
+
+def getKey(record):
+    ar = record.split("${")
+    referenceData = ar[0]
+    arra = referenceData.split("$")
+    chromo = arra[5].split(":")[1]
+    position = arra[6].split(":")[1]
+    return (chromo,position)
+
+def getNumbers(record):
+    ar = record.split("${")
+    referenceData = ar[0]
+    alignmentsData = ar[1]
+    arra = referenceData.split("$")
+    chromo = arra[5].split(":")[1]
+    position = arra[6].split(":")[1]
+    repetitiveSequenceLength = getReferenceRepeatLength(referenceData)
+    unitLength = getReferenceUnitLength(referenceData)
+    fullRepetitiveSequenceLength = (int(repetitiveSequenceLength)/int(unitLength))*unitLength
+    delta = repetitiveSequenceLength-fullRepetitiveSequenceLength
+    return printNumbers(referenceData, alignmentsData, getReferenceUnitLength(referenceData))
+
+def printNumbers(refD, aliD, unitLength):
+    array = refD.split("$")
+    unit = array[0].split(":")[1]
+    exactVals = []
+    lowerVals = []
+    for element in  aliD.split("]#"):
+        if len(element)>2:
+            array = element[1:].split("$")
+            left = array[0].split(":")[1]
+            right = array[1].split(":")[1]
+            numOfRepeats = len(array[2].split(":")[1])/unitLength
+            if len(left)<10 or len(right)<10:
+                lowerVals.append(numOfRepeats)
+            else:
+                exactVals.append(numOfRepeats)
+    return (sorted(exactVals),sorted(lowerVals))
+
+    
 dataDictionary = {}
-listOfMergedFiles = ['/home/gabdank/Documents/STR_Attempt/Simulation2/merged.output','/home/gabdank/Documents/STR_Attempt/Simulation2/merged.output']
+listOfMergedFiles = ['merged.output','merged.output']
+
 initRefDict(dataDictionary,listOfMergedFiles,20)
+
+# at this stage we have finished to extract all the reference information and now we should extract strain specific information
 
 for entry in dataDictionary:
     print entry
     print dataDictionary[entry]
+
+strainDictionary = {}
+strainDictionary["sampleStrain"]={}
+strainFileName = "merged.output"
+processNumbers(strainFileName, dataDictionary, "sampleStrain", strainDictionary["sampleStrain"])
+
     
 '''def initiateReferenceDictionary(intDict, listOfFileNames, flankLengthThreshold):
     for name in listOfFileNames:
