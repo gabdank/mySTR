c9cea994601abee4b7a39d2a5380a9013af0c8ee
Idan Gabdank <gabdank@stanford.edu>
2015-05-06 17:08:52 -0700

    right side flank seems to cause a problem

--- src/utils/bedFileFilter.java
+++ src/utils/bedFileFilter.java
@@ -44,34 +44,40 @@ public class bedFileFilter {
 				String representativeUnit = SequenceUtils.getRepresentative(unit);
 
 				
-				int repeatLength = end-start+1;
+				//int repeatLength = end-start+1;
+				//System.out.println(repLength + "?=?"+repeatLength);
 				
-				System.out.println(line);
-				
-				String STR_Region = sequence.substring(startInside-1, startInside+repeatLength);
+				//System.out.println(line);
+				if (startInside+repLength==769){
+					System.out.println(line);
+					System.out.println(sequence.substring(startInside-1, startInside+repLength));
+					System.out.println((startInside-1)+"\t"+(startInside+repLength)+"\t"+sequence.length());
+				}
+				String STR_Region = sequence.substring(startInside-1, startInside+repLength);
 				String leftFlank = sequence.substring(0, startInside-1);
-				String rightFlank = sequence.substring(startInside+repeatLength-2);
-				//System.out.println("-----------------");
-				//System.out.println(STR_Region);				
-				//System.out.println(leftFlank);				
-				//System.out.println(rightFlank);
-				//System.out.println("-----------------");
+				String rightFlank = sequence.substring(startInside+repLength-2);
+				
 				
 				if (leftFlank.length()>30){
 					leftFlank = leftFlank.substring(leftFlank.length()-30);
-					System.out.println(leftFlank);
+					//System.out.println(leftFlank);
 				}
 				if (rightFlank.length()>30){
 					rightFlank = rightFlank.substring(0,30);
-					System.out.println(rightFlank);
+					//System.out.println(rightFlank);
 				}
+				// Creating the combined sequence - that contains flanks and the STR region between them
 				String combinedRepeat = leftFlank+STR_Region+rightFlank;
+				int startPosition = leftFlank.length();
+				int endPosition = startPosition+STR_Region.length();
 				
-				System.out.println("REFERENCE:");
-				System.out.println(leftFlank+">>"+STR_Region+"<<"+rightFlank);
-				//System.out.println("COMBINED:");
-				//System.out.println(combinedRepeat);
-				System.out.println("================================");
+				//System.out.println("REFERENCE:");
+				//System.out.println(leftFlank+">>"+STR_Region+"<<"+rightFlank);
+				
+				//System.out.println("Updated Start = "+startPosition+ " Updated  End = "+ endPosition);
+				
+				
+				
 				
 				RepetitionList repetition1 = new RepetitionList();
 				FindRepetition find_repetition= new FindRepetition();
@@ -80,16 +86,36 @@ public class bedFileFilter {
 				find_repetition.extendMaximalRepetition("sample_id", combinedRepeat, repetition1);
 				if (repetition1.get_max_repetition_unit_size()>1 && repetition1.get_max_repetition_unit_size()<9 && repetition1.get_max_repetition_length()>16){
 					String repeat1 = combinedRepeat.substring(repetition1.get_max_repetition_left_index(),repetition1.get_max_repetition_left_index()+repetition1.get_max_repetition_unit_size());
-					//System.out.println("Repeat = "+repeat1);
-					//System.out.println(repetition1.get_max_repetition_length());
 					String repeatSection = combinedRepeat.substring(repetition1.get_max_repetition_left_index(),repetition1.get_max_repetition_right_index() );
 					
-					System.out.println(combinedRepeat.substring(0,repetition1.get_max_repetition_left_index())+"**"+repeatSection+"**"+combinedRepeat.substring(repetition1.get_max_repetition_right_index()));
+					int startPositionNew = repetition1.get_max_repetition_left_index();
+					int endPositionNew = repetition1.get_max_repetition_right_index();
 					
-					//System.out.println(start+repetition1.get_max_repetition_left_index()-leftFlank.length());
-					//System.out.println(start+repetition1.get_max_repetition_right_index()-leftFlank.length());
+					//System.out.println("New Updated Start = "+startPositionNew+ " New Updated  End = "+ endPositionNew);
+					
+					int startDelta = startPositionNew-startPosition; // check if it shouldn't be the other way around
+					int endDelta = endPositionNew-endPosition;
+					
+					//System.out.println("Start delta = "+startDelta+" End delta = "+endDelta);
+					
+					//System.out.println(combinedRepeat.substring(0,repetition1.get_max_repetition_left_index())+"**"+repeatSection+"**"+combinedRepeat.substring(repetition1.get_max_repetition_right_index()));
+					
+					//System.out.println(chromosome+"\t"+(start+startDelta) + "\t" +(end+endDelta));
+										
+					bw1.write(chromosome+"\t"+(start+startDelta)+"\t"+(end+endDelta)+"\t"+repeat1+"\t"+sequence+"\t"+repeatSection.length()+"\t"+(startInside+startDelta)+"\n");
+					
 				}
-				System.out.println("================================");
+				/*else{
+					System.out.println("WE HAVE A PROBLEM");
+					System.out.println(line);
+					System.out.println(repetition1.get_max_repetition_length());
+					System.out.println(repetition1.get_max_repetition_unit_size());
+					String repeat1 = combinedRepeat.substring(repetition1.get_max_repetition_left_index(),repetition1.get_max_repetition_left_index()+repetition1.get_max_repetition_unit_size());
+					System.out.println(repeat1);
+					throw new IOException("ZOPA");
+				}*/
+			
+				//System.out.println("================================");
 					
 					
 				
@@ -99,15 +125,17 @@ public class bedFileFilter {
 				 * 
 				 */
 				
-			if (mone==3)
-				break;
+			//if (mone==3)
+			//	break;
 			mone ++;
 				
-			//if (mone%100000==0) 
-			//	System.out.println("PROCESSED "+mone +" lines in BED");
-			//mone++;
+			if (mone%100000==0) 
+				System.out.println("PROCESSED "+mone +" lines in BED");
+			mone++;
 			}
 		}
+		bw1.close();
+		fw1.close();
 
 	}
 
